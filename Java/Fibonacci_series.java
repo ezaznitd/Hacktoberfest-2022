9bf716efd1954bc49e01b6a9577f2da4112254c3
Sameer Tripathi <105990292+sam-tripathi@users.noreply.github.com>
2022-10-03 18:50:51 +0000

    added Fibonacci_series

---
+++ Java/Fibonacci_series.java
@@ -0,0 +1,16 @@
+// just a java program to print Fibonacci_series by sam-tripathi
+class Fibonacci_series{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+}
+}  
